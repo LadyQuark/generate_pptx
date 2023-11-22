47d565a602a783b923aa461da6f29afeb04f5610
Dipika Bothra <dips@Dipikas-iMac.local>
2023-11-22 11:41:11 +0530

    Added set_slide_size. copy_slide_to_other_presentation only changes ppt size if destination is empty

--- presentationmanager.py
+++ presentationmanager.py
@@ -31,8 +31,7 @@ class PresentationManager(object):
 
         if slide_size:
             height, width = slide_size
-            self.presentation.slide_height = height
-            self.presentation.slide_width = width
+            self.set_slide_size(height, width)
 
         # Setting index of slide to be used as a template
         self.template_slide_index = template_slide_index
@@ -53,6 +52,10 @@ class PresentationManager(object):
     @property
     def total_slides(self):
         return len(self.presentation.slides)
+    
+    def set_slide_size(self, height, width):
+        self.presentation.slide_height = height
+        self.presentation.slide_width = width        
 
     def duplicate_slide(self, index, destination=None):
         """
@@ -164,8 +167,13 @@ class PresentationManager(object):
 
     @classmethod
     def copy_slide_to_other_presentation(cls, source, dest_filepath, slides_to_copy=[]):
-        height, width = source.presentation.slide_height, source.presentation.slide_width
-        destination = PresentationManager(dest_filepath, slide_size=(height, width))
+        # Load presentation
+        destination = PresentationManager(dest_filepath)
+        # Copy presentation size if destination is empty
+        if destination.total_slides == 0:
+            height, width = source.presentation.slide_height, source.presentation.slide_width
+            destination.set_slide_size(height, width)
+        # If no slide numbers given, default to all slides
         if not slides_to_copy:
             slides_to_copy = range(source.total_slides)
         try:
